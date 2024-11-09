624826c3a9464f7a41d94a1039796281dfe5e694
Taufiq Sadri <taufiqsadri@gmail.com>
2024-11-09 13:33:11 +0700

    menambahkan fitur search dan produk

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//toggle class active
+//toggle class active unutuk hamburger menu
 
 const navbarNav = document.querySelector('.navbar-nav');
 //ketika hamburger menu di klik
@@ -7,11 +7,63 @@ document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
-// klik di luar side bar untuk menghilangkan nav
+// Menambahkan kelas active pada searcform
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('.search-box');
 
-const hamburger = document.querySelector('#hamburger-menu');
+document.querySelector('#search-button').onclick = (e) => {
+    e.preventDefault();
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+
+}
+const shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+    e.preventDefault();
+    shoppingCart.classList.toggle('active');
+};
+// klik di luar  element
+
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button')
 document.addEventListener('click', function (e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
     }
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
+    if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove('active');
+    }
 });
+
+
+// modal box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    }
+})
+
+
+
+// kil tombol close 
+
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none'
+    e.preventDefault();
+}
+
+//klik di luar modal
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        itemDetailModal.style.display = 'none';
+    }
+};
